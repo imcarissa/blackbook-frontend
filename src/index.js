2d72f24685bfb97b726ec430c7ea3a741b5bde58
imcarissa <carissa.sarreal@gmail.com>
2021-07-22 14:09:21 -0400

    files imported to be able to set up store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleWare } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import App from './App';
+
+// set up store
+
 
 
 ReactDOM.render (<App />, document.getElementById('root'));
