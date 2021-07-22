c02f6c0ba2a338f7e648a302b0042a6348018e0d
imcarissa <carissa.sarreal@gmail.com>
2021-07-22 12:25:20 -0400

    test fetch for App.js to render backend api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-class App extends React.Component() {
+class App extends React.Component {
+  
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/groups/2')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
+
   render() {
     return (
       <div className="App">
