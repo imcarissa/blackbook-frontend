0aaf3b52dfef0361aaf9e8c59e07e0d49a07bcc0
imcarissa <carissa.sarreal@gmail.com>
2021-07-23 16:59:30 -0400

    swtich case statement implemented

--- src/reducers/groupReducer.js
+++ src/reducers/groupReducer.js
@@ -1,5 +1,10 @@
 export default function groupReducer(state = {groups: []}, action) {
 
-
+    switch (action.type) {
+        case 'FETCH_GROUPS':
+            return {groups: action.payload}
+        default:
+            return state
+    }
 
 }
