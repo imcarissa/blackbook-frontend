66177177e6d22341333dcbdbfd7f22a0c6613b05
imcarissa <carissa.sarreal@gmail.com>
2021-07-23 16:56:52 -0400

    name changed to GroupInput from GroupInputForm

---
+++ src/components/GroupsInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class GroupsInput extends React.Component {
+    render () {
+        return (
+            <div>
+                GroupsInputForm
+            </div>
+        )
+    }
+}
+
+export default GroupsInput
