45bb3c185f327baabf6551c68c913369da9620f3
imcarissa <carissa.sarreal@gmail.com>
2021-07-23 16:18:56 -0400

    components folder created

---
+++ src/components/Groups.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import GroupsContainer from '../containers/GroupsContainer'
+
+
+const Accounts = (props) => {
+    return (
+        <div>
+            Groups
+        </div>
+    )
+
+
+
+
+}
+
+export default Groups
---
+++ src/components/GroupsInputForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class GroupsInputForm extends React.Component {
+    render () {
+        return (
+            <div>
+                GroupsInputForm
+            </div>
+        )
+    }
+}
+
+export default GroupsInputForm
