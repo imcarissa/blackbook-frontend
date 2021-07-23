dd3b6787f50aa19237da3b41433d36fee379ca5e
imcarissa <carissa.sarreal@gmail.com>
2021-07-23 16:59:02 -0400

    return (dispatch) implemented

--- src/actions/fetchGroups.js
+++ src/actions/fetchGroups.js
@@ -1,6 +1,10 @@
 export function fetchGroups() {
-    fetch('http://localhost:3000/api/v1/groups')
-    .then(response => response.json())
-    .then(data => console.log(data))
-
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/groups')
+        .then(response => response.json())
+        .then(groups => dispatch({
+            type: 'FETCH_GROUPS',
+            payload: groups
+        }))   
+    }
 }
