cfcbbde469a0f2882d05bf37e170e1c3c896d5ab
imcarissa <carissa.sarreal@gmail.com>
2021-07-25 22:20:44 -0400

    addGroup action created for POST fetch

--- src/actions/addGroup.js
+++ src/actions/addGroup.js
@@ -1,9 +1,15 @@
 export const addGroup = (data) => {
 
     return (dispatch) => {
-
+        fetch('http://localhost:3000/api/v1/accounts', {
+            headers: {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(data)
+            }
+        })
+        .then(response => response.json())
+        .then(group => dispatch({type: 'ADD_GROUP', payload: group}))
     }
-
-
 
 }
