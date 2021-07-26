b707301be7f1340fdad10a15ed27305fc900373c
imcarissa <carissa.sarreal@gmail.com>
2021-07-25 22:23:01 -0400

    switch case ADD_GROUP added when POST fetch submits form

--- src/reducers/groupReducer.js
+++ src/reducers/groupReducer.js
@@ -3,6 +3,8 @@ export default function groupReducer(state = {groups: []}, action) {
     switch (action.type) {
         case 'FETCH_GROUPS':
             return {groups: action.payload}
+        case 'ADD_GROUP':
+            return {...state, groups: [...state.groups, action.payload]}
         default:
             return state
     }
