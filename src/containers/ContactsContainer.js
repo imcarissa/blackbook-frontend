e152ec140a954ababad8df297ce7c553f59dccea
imcarissa <carissa.sarreal@gmail.com>
2021-07-28 15:53:01 -0400

    Contacts imported for container to list array of contacts

--- src/containers/ContactsContainer.js
+++ src/containers/ContactsContainer.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ContactsInput from '../components/ContactsInput'
+import Contacts from '../components/Contacts'
 
 
 class ContactsContainer extends React.Component {
+
+
 
     render() {
         return (
             <div>
                 <ContactsInput/>
+                <Contacts contacts={this.props.group && this.props.group.contacts}/>
             </div>
         )
     }
