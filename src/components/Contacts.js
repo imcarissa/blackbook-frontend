4f4ff230d2c6508e3d55ab4989d2e8ad0c9710db
imcarissa <carissa.sarreal@gmail.com>
2021-07-28 15:52:11 -0400

    created, code for rendering started

---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Contacts = (props) => {
+    
+    return (
+        <div>
+            - list of contacts is here -
+        </div>
+    )
+}
+
+export default Contacts
