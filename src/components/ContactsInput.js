45b82f1facccaaa22f14a4b6814b21d190e9f1f8
imcarissa <carissa.sarreal@gmail.com>
2021-07-28 17:45:52 -0400

    handleChange and handleSubmit implemented on form

--- src/components/ContactsInput.js
+++ src/components/ContactsInput.js
@@ -1,15 +1,46 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
 
 class ContactsInput extends React.Component {
+
+    state = {
+        name: '',
+        email: '',
+        phone_number: '',
+        notes: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+            
+    }
 
     render() {
         return (
             <div>
                 - the Contacts Input Form here -
+                <form onSubmit={this.handleSubmit}>
+                ****    <select name="category" value={this.state.name} onChange={this.handleChange}>
+                        <option>category</option> 
+                    </select> **** 
+                    <br/>
+
+                    <label>Name:</label>
+                    <input type="text" name="name" value={this.state.name}/><br/>
+                    <label>Phone Number:</label><input type="text"/><br/>
+                    <label>Email:</label><input type="text"/><br/>
+                    <label>Notes:</label><input type="text"/><br/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
 }
 
-export default ContactsInput
+export default connect(null)(ContactsInput)
