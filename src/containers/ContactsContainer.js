7120d9a493cff2e454cfdf133db37b2108e3870d
imcarissa <carissa.sarreal@gmail.com>
2021-07-28 14:42:48 -0400

    Contacts Container created with starting code

---
+++ src/containers/ContactsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class ContactsContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                ContactsContainer
+            </div>
+            )
+        }
+}
