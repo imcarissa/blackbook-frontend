8f0fdbbc986f2df73bc4d30fe55da2b6227c16e7
imcarissa <carissa.sarreal@gmail.com>
2021-07-30 04:40:02 -0400

    case EDIT_GROUP created

--- src/reducers/groupReducer.js
+++ src/reducers/groupReducer.js
@@ -1,10 +1,13 @@
 export default function groupReducer(state = {groups: []}, action) {
     
     switch (action.type) {
+        
         case 'FETCH_GROUPS':
             return {groups: action.payload}
+        
         case 'ADD_GROUP':
             return {...state, groups: [...state.groups, action.payload]}
+        
         case 'ADD_CONTACT':
             let groups = state.groups.map(group => {
                 if (group.id === action.payload.id) {
@@ -15,7 +18,29 @@ export default function groupReducer(state = {groups: []}, action) {
                 }
             })
             return {...state, groups: groups}
+
+        case 'EDIT_GROUP': 
+        let editGroups = state.groups.map(group => {
+            if (group.id === action.payload.id) {
+                return action.payload
+            }
+            else {
+                return group
+            }
+        })
+            return {...state, groups: editGroups}
         
+        case 'DELETE_CONTACT':
+            let deleteGroups = state.groups.map(group => {
+                if (group.id === action.payload.id) {
+                    return action.payload
+                }
+                else {
+                    return group
+                }
+            })
+            return {...state, groups: deleteGroups}
+       
         default:
             return state
     }
