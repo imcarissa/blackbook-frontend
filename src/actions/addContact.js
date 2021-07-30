c3f88bfd9e8254dcb0cb544953c279e484768399
imcarissa <carissa.sarreal@gmail.com>
2021-07-29 21:21:33 -0400

    fetch to endpoint implemented

--- src/actions/addContact.js
+++ src/actions/addContact.js
@@ -1,1 +1,14 @@
-import React from 'react'
+export const addContact = (contact, groupId) => {
+
+    return (dispatch) => {
+        fetch(`http://localhost:3000/api/v1/accounts/${groupId}/contacts`, {
+            headers: {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(contact)
+            }
+        })
+        .then(response => response.json())
+        .then(group => dispatch({type: 'ADD_CONTACT', payload: group}))
+    }
+}
