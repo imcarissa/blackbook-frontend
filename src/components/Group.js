1ebff3658bf431b4bbb1d33826d6adcc5f6a14b6
imcarissa <carissa.sarreal@gmail.com>
2021-07-30 04:39:03 -0400

    Edit feature for groups rendered

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
+import GroupEdit from './GroupEdit'
 import ContactsContainer from '../containers/ContactsContainer'
 
 
 const Group = (props) => {
 
-    //let group = props.groups[props.match.params.id - 1]
     let group = props.groups.filter(group => group.id == props.match.params.id)[0]
 
     return (
@@ -14,6 +14,7 @@ const Group = (props) => {
           {group ? group.name : null} -  {group ? group.description : null}
         </h2>
         <ContactsContainer group={group}/>
+        <GroupEdit group={group}/>
       </div>
     )
 }
