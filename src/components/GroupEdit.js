27eb4b4909cd8dadc09c7e366c2e7553b835fcd9
imcarissa <carissa.sarreal@gmail.com>
2021-07-30 04:38:28 -0400

    group can now be editted

---
+++ src/components/GroupEdit.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { editGroup } from '../actions/editGroup'
+
+
+class GroupEdit extends React.Component {
+
+    state = {
+        name: '',
+        category: '',
+        description: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        let group = {...this.state, id: this.props.group.id}
+        this.props.editGroup()
+        this.setState({
+            name: '',
+            category: '',
+            description: ''
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Create A Group: </label>
+                    <input type='text' placeholder='name of group' value={this.state.name} name='name' onChange={this.handleChange}/>
+                    <br/>
+                    <label>Category: </label>
+                    <input type='text' placeholder='category' value={this.state.category} name='category' onChange={this.handleChange} />
+                    <br/>
+                    <label>Description: </label>
+                    <input type='text' placeholder='description' value={this.state.description} name='description' onChange={this.handleChange} />
+                    <br/>
+                    <input type="submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default connect(null, {editGroup})(GroupEdit)
