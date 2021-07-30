2914751209a9ce98253cb4ceec0524cad3f2b007
imcarissa <carissa.sarreal@gmail.com>
2021-07-29 22:26:50 -0400

    ADD_CONTACT code updated to map groups by ID and update the group upon adding a new contact

--- src/reducers/groupReducer.js
+++ src/reducers/groupReducer.js
@@ -1,12 +1,21 @@
 export default function groupReducer(state = {groups: []}, action) {
-
+    
     switch (action.type) {
         case 'FETCH_GROUPS':
             return {groups: action.payload}
         case 'ADD_GROUP':
             return {...state, groups: [...state.groups, action.payload]}
         case 'ADD_CONTACT':
-            return {}
+            let groups = state.groups.map(group => {
+                if (group.id === action.payload.id) {
+                    return action.payload
+                }
+                else {
+                    return group
+                }
+            })
+            return {...state, groups: groups}
+        
         default:
             return state
     }
